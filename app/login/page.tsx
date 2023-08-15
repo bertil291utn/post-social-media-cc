2c78e4d3ff83b444ea1b19ce21fa7c324620d490
Bertil Tandayamo <bertilt4@gmail.com>
2023-08-15 18:40:01 -0500

    Add username input  fgor sign in /up

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Button from '@components/common/Button/Button.component';
 import { TERTIARY } from '@components/common/Button/button.helper';
 import Card from '@components/common/Card.component';
@@ -10,17 +12,26 @@ const Login = () => {
   }
 
   return (
-    <div className={`h-screen flex items-center justify-center w-1/2 mx-auto`}>
-      <Card >
-        <div className='w-full mx-auto'>
+    <div className={`flex items-center justify-center w-1/2 mx-auto`}>
+      <Card>
+        <div className='w-full mx-auto my-2'>
+
+          <div className="sm:col-span-4 mb-4">
+            <div className="mt-2">
+              <div className="flex rounded-md shadow-sm ring-1 ring-inset focus:ring-0 ring-gray-300  sm:max-w-md">
+                <input type="text" name="username" id="username" autoComplete="username" className="
+                block flex-1 border-0 bg-transparent p-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="janesmith" />
+              </div>
+            </div>
+          </div>
+
           <Button
-            buttonType={TERTIARY}
-            className={`relative text-center`}
+            className={`text-center `}
             onClick={LoginAction}
           >
-            <FcGoogle className={`absolute text-lg`} />
-            <span className='block'>
-              Sign in with Google
+            {/* <FcGoogle className={`absolute text-lg`} /> */}
+            <span className='block text-white'>
+              Sign in/up
             </span>
           </Button>
         </div>
