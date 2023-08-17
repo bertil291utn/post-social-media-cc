5131431f9d2b03045f0b49dc196b4c8274a2c8d6
Bertil Tandayamo <bertilt4@gmail.com>
2023-08-17 09:27:31 -0500

    Rename directory path and also move to this new service directory

--- app/components/Homepage.component.tsx
+++ app/components/Homepage.component.tsx
@@ -2,7 +2,7 @@
 
 import PostLayout from '@components/Post/layouts/Post.layout';
 import PostProvider from 'context/Post.context';
-import { getAllPosts } from 'data/fetchPosts.data';
+import { getAllPosts } from 'services/fetchPosts.service';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { initialSetPost, setIsLoading } from 'redux/Post/Post.reducer';
@@ -13,6 +13,8 @@ const HomePage = () => {
   const { loading, error, data } = getAllPosts();
 
   useEffect(() => {
+    //TODO_LATER: check later after is not getting data properly, stucking data as undefined 
+    //it might need to add a 10 seccs delay and thwn display as empty posts
     if (data?.posts) {
       dispatch(initialSetPost(data?.posts))
       dispatch(setIsLoading(loading))
