aae1f19814336b02b2085b9b3b8a14ea3f4177fa
Bertil Tandayamo <bertilt4@gmail.com>
2023-08-17 09:38:06 -0500

    Addd modification to check if user exists before store

--- app/services/saveUser.service.ts
+++ app/services/saveUser.service.ts
@@ -1,9 +1,18 @@
-import { gql, useMutation } from "@apollo/client";
+import { gql, useMutation, useQuery } from "@apollo/client";
 import { User } from '@interfaces/User';
 
-export const saveUser = (body: User) => {
+export const saveUser = async (body: User) => {
   if (!body) throw new Error('Canot be empty body');
   if (!Object.keys(body).length) throw new Error('Canot be empty body');
+
+  const CHECK_EXISTING_USER_QUERY = gql`
+    query CheckExistingUser($username: String) {
+      users(username: $username) {
+        username
+      }
+    }
+  `;
+
 
   const SAVE_USER_QUERY = gql`
     mutation AddUser($avatarURL: String = "", $id: ID = "", $name: String = "", $username: String = "") {
@@ -22,6 +31,19 @@ export const saveUser = (body: User) => {
 
 
   const [addUserMutation, { loading, error }] = useMutation(SAVE_USER_QUERY);
-  return addUserMutation({ variables: body });
+
+  const { username } = body;
+
+  const { data } = await useQuery(CHECK_EXISTING_USER_QUERY, {
+    variables: { username },
+  });
+
+  if (data?.users.length) {
+    console.log("User already exists");
+    return;
+  }
+
+  const variables = { ...body };
+  return addUserMutation({ variables });
 
 }
