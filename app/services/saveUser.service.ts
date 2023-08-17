3d85e67ef1c079ebe9abbb8cf8ceb8d6d2b0f682
Bertil Tandayamo <bertilt4@gmail.com>
2023-08-17 09:30:23 -0500

    Add initial conditions to save user mutation

--- app/services/saveUser.service.ts
+++ app/services/saveUser.service.ts
@@ -2,6 +2,9 @@ import { gql, useMutation } from "@apollo/client";
 import { User } from '@interfaces/User';
 
 export const saveUser = (body: User) => {
+  if (!body) throw new Error('Canot be empty body');
+  if (!Object.keys(body).length) throw new Error('Canot be empty body');
+
   const SAVE_USER_QUERY = gql`
     mutation AddUser($avatarURL: String = "", $id: ID = "", $name: String = "", $username: String = "") {
       createUsers(
@@ -19,6 +22,6 @@ export const saveUser = (body: User) => {
 
 
   const [addUserMutation, { loading, error }] = useMutation(SAVE_USER_QUERY);
-  return addUserMutation({ variables:body });
+  return addUserMutation({ variables: body });
 
 }
