74c6d20adb8c792bf21fced7e552ae0a6ad7b2bb
Bertil Tandayamo <bertilt4@gmail.com>
2023-08-17 18:54:02 -0500

    Add map option to set isliked according to liked posts from current user

--- app/components/Homepage.component.tsx
+++ app/components/Homepage.component.tsx
@@ -4,21 +4,33 @@ import PostLayout from '@components/Post/layouts/Post.layout';
 import PostProvider from 'context/Post.context';
 import { getAllPosts } from 'services/fetchPosts.service';
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { initialSetPost, setIsLoading } from 'redux/Post/Post.reducer';
+import { Post } from '@interfaces/Post';
+import { LoginSelector } from 'redux/Login/Login.selector';
 
 const HomePage = () => {
   const dispatch = useDispatch();
 
   const { loading, error, data } = getAllPosts();
+  const login = useSelector(LoginSelector);
+  const user = login.user
 
   useEffect(() => {
     //TODO_LATER: check later after is not getting data properly, stucking data as undefined 
     //it might need to add a 10 seccs delay and thwn display as empty posts
-    if (data?.posts) {
-      dispatch(initialSetPost(data?.posts))
-      dispatch(setIsLoading(loading))
+    if (!data?.posts) return;
+    let posts = data?.posts as Array<Post>
+    if (!user.likedPosts!.length) {
+      posts = posts.map(p => ({ ...p, isLiked: false }))
+
     }
+    if (user.likedPosts!.length) {
+      const likedPostsIdArr = user.likedPosts!.map(p => p.id)
+      posts = posts.map(p => ({ ...p, isLiked: likedPostsIdArr.includes(p.id) }))
+    }
+    dispatch(initialSetPost(posts))
+    dispatch(setIsLoading(loading))
   }, [data])
 
   return (
